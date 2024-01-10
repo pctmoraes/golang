3447ae473df3a7ad54005fcef6f612db7d8a475f
pctmoraes <paula.ct.moraes@gmail.com>
2024-01-10 15:09:05 -0300

    Learning about third-party libraries

--- about packages/go.mod
+++ about packages/go.mod
@@ -1,3 +1,5 @@
 module about_packages
 
 go 1.21.6
+
+require github.com/badoux/checkmail v1.2.1
--- about packages/main.go
+++ about packages/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"about_packages/my_packages"
 	"fmt"
+	"about_packages/my_packages"
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("I am main print")
 	a_package.FirstPackagePrint()
 	a_package.SecondPackagePrint()
+
+	// Below is the second exercise about packages
+	// where i am going to use a third-party library
+	// here is the library link: https://github.com/badoux/checkmail
+	// it's a Golang package for email validation
+	checkmail_error := checkmail.ValidateFormat("my_email@email.com")
+	
+	if checkmail_error != nil {
+		fmt.Println(checkmail_error)
+	}
+		
 }
