ac8dda06d8eeb6cbf8740338dc6afc97293e8e56
pctmoraes <paula.ct.moraes@gmail.com>
2024-01-12 18:41:53 -0300

    Add the func sumAndEvenOrOdd as a func with more than one value exercise

--- 3_functions/sum.go
+++ 3_functions/sum.go
@@ -6,6 +6,20 @@ func sum(n1, n2 int64) int64 {
 	return n1 + n2
 }
 
+func sumAndEvenOrOdd(n1, n2 int64) (int64, string) {
+	sum := n1 + n2
+
+	var evenOrOdd string
+
+	if sum % 2 == 0 {
+		evenOrOdd = "even"
+	} else {
+		evenOrOdd = "odd"
+	}
+
+	return sum, evenOrOdd
+}
+
 func main() {
 	var num1 int64
 	var num2 int64
@@ -19,5 +33,9 @@ func main() {
 	the_sum := sum(num1, num2)
 	fmt.Print(the_sum)
 
-	
+	// The function below will return the sum and if the number is even or not
+	// It is a modified version of sum() to 
+	//understand how a func can return more than one value
+	sum, evenOrOdd := sumAndEvenOrOdd(num1,num2)
+	fmt.Println("The sum is:", sum, "and it is", evenOrOdd)
 }
