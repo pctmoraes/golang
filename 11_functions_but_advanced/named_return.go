4c8e514377986bffa51628defdc468e24aa25727
pctmoraes <paula.ct.moraes@gmail.com>
2024-01-19 09:19:19 -0300

    Add a brief description on named return

--- 11_functions_but_advanced/named_return.go
+++ 11_functions_but_advanced/named_return.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+// a named return value is a feature that allows you 
+// to declare and name the return variables in the function signature
+// this simplifies the code by making it clear what each return variable represents
+// it also has a special impact on the return behavior
+
 func sumAndSubtract(n1, n2 int) (sum int, subtract int) {
 	sum = n1 + n2
 	subtract = n1 - n2
