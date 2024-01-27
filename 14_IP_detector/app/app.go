90923877de3de3cf1fb4c2feba03e7aa91e945ad
pctmoraes <paula.ct.moraes@gmail.com>
2024-01-27 18:54:27 -0300

    Add app.commands and getIP function to app.go

--- 14_IP_detector/app/app.go
+++ 14_IP_detector/app/app.go
@@ -1,6 +1,9 @@
 package app
 
 import (
+	"fmt"
+	"log"
+	"net"
 	"github.com/urfave/cli"
 )
 
@@ -10,6 +13,36 @@ func Detect() *cli.App {
 	app.Name = "IP Detector"
 	app.Usage = "IP Detector will return the server IP address searched"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "ip",
+			Usage:  "Return IP addresses",
+			Flags:  flags,
+			Action: getIP,
+		},
+		
+	}
+
 	return &app
-	
+}
+
+func getIP(c *cli.Context) {
+	host := c.String("host")
+
+	ips, _error := net.LookupIP(host)
+
+	if _error != nil {
+		log.Fatal(_error)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
